39e99249781d263dd07a65a7bff26dbe52f5e50f
ajshemi <akpojotor@yahoo.com>
2021-02-01 12:49:30 -0500

    upated application controller and ibm.rb

--- config/initializers/ibm.rb
+++ config/initializers/ibm.rb
@@ -1,19 +1,19 @@
 # frozen_string_literal: true
 
-# require "json"
-# require "ibm_watson/authenticators"
-# require "ibm_watson/natural_language_understanding_v1"
-# # Access tokens are valid for approximately one hour and must be regenerated.?
-# # If using IAM
-# include IBMWatson
-# authenticator = IBMWatson::Authenticators::IamAuthenticator.new(
-#   apikey:ENV['NATURAL_LANGUAGE_UNDERSTANDING_APIKEY']
-# )
-# NLU = IBMWatson::NaturalLanguageUnderstandingV1.new(
-#   authenticator: authenticator,
-#   version: "2019-07-12"
-# )
-# NLU.service_url = ENV['NATURAL_LANGUAGE_UNDERSTANDING_URL']#"https://api.us-east.natural-language-understanding.watson.cloud.ibm.com/instances/21007ec0-edd4-43f7-a8d6-3b61d569b4ac"
+require "json"
+require "ibm_watson/authenticators"
+require "ibm_watson/natural_language_understanding_v1"
+# Access tokens are valid for approximately one hour and must be regenerated.?
+# If using IAM
+include IBMWatson
+authenticator = IBMWatson::Authenticators::IamAuthenticator.new(
+  apikey:ENV['NATURAL_LANGUAGE_UNDERSTANDING_APIKEY']
+)
+NLU = IBMWatson::NaturalLanguageUnderstandingV1.new(
+  authenticator: authenticator,
+  version: "2019-07-12"
+)
+NLU.service_url = ENV['NATURAL_LANGUAGE_UNDERSTANDING_URL']#"https://api.us-east.natural-language-understanding.watson.cloud.ibm.com/instances/21007ec0-edd4-43f7-a8d6-3b61d569b4ac"
 
 # response=User.all.map{|user| 
 # natural_language_understandingNLUNLU.analyze(text:user.comments.map{|comment|comment.user_comment}.join(" "), features: {
